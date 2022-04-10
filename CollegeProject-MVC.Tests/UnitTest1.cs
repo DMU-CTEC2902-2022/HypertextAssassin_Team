a6d712e0220bd9ba8e55674db2706598af514dd9
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-04-10 09:42:00 +0100

    Week 21 Done

---
+++ CollegeProject-MVC.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CollegeProject_MVC.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
