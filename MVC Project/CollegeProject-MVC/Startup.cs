c39331c0f39b1368c1e7781a658dfde87c272527
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-04-10 06:10:48 +0100

    Skeleton Added

---
+++ MVC Project/CollegeProject-MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CollegeProject_MVC.Startup))]
+namespace CollegeProject_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
