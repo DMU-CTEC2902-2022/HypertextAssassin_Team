57231bbe602943e3df866cdee3f9ab9822a474cf
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-04-15 11:54:14 +0100

    Testing

--- Project/CollegeProjectMVC.Tests/Controllers/CourseControllerTest.cs
+++ Project/CollegeProjectMVC.Tests/Controllers/CourseControllerTest.cs
@@ -24,5 +24,83 @@ namespace CollegeProjectMVC.Tests.Controllers
             // Assert
             Assert.AreEqual("Computer Science", result.CourseTitle);
         }
+        [TestMethod]
+        public void CourseCode()
+        {
+            // Arrange
+            CourseController controller = new CourseController();
+
+            //Act
+            ViewResult viewResult = controller.Course();
+            CourseModel result = viewResult.Model as CourseModel;
+
+            // Assert
+            Assert.AreEqual("D16", result.CourseCode);
+        }
+        [TestMethod]
+        public void CourseDescription()
+        {
+            // Arrange
+            CourseController controller = new CourseController();
+
+            //Act
+            ViewResult viewResult = controller.Course();
+            CourseModel result = viewResult.Model as CourseModel;
+
+            // Assert
+            Assert.AreEqual("Computer science (CS) is the study of computers and algorithmic processes, including their principles, their hardware and software designs, their applications, and their impact on society.", result.CourseDescription);
+        }
+        [TestMethod]
+        public void CourseDuration()
+        {
+            // Arrange
+            CourseController controller = new CourseController();
+
+            //Act
+            ViewResult viewResult = controller.Course();
+            CourseModel result = viewResult.Model as CourseModel;
+
+            // Assert
+            Assert.AreEqual(390, result.CourseTotalHours);
+        }
+        [TestMethod]
+        public void CourseAvailability()
+        {
+            // Arrange
+            CourseController controller = new CourseController();
+
+            //Act
+            ViewResult viewResult = controller.Course();
+            CourseModel result = viewResult.Model as CourseModel;
+
+            // Assert
+            Assert.AreEqual("Available", result.CourseAvailability);
+        }
+        [TestMethod]
+        public void CourseAccreditation()
+        {
+            // Arrange
+            CourseController controller = new CourseController();
+
+            //Act
+            ViewResult viewResult = controller.Course();
+            CourseModel result = viewResult.Model as CourseModel;
+
+            // Assert
+            Assert.AreEqual("BCS", result.Accreditation);
+        }
+        [TestMethod]
+        public void CourseCost()
+        {
+            // Arrange
+            CourseController controller = new CourseController();
+
+            //Act
+            ViewResult viewResult = controller.Course();
+            CourseModel result = viewResult.Model as CourseModel;
+
+            // Assert
+            Assert.AreEqual("16,250$", result.CourseCost);
+        }
     }
 }
--- Project/CollegeProjectMVC.Tests/Controllers/ModuleUnitTest.cs
+++ Project/CollegeProjectMVC.Tests/Controllers/ModuleUnitTest.cs
@@ -1,25 +1,31 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CollegeProjectMVC;
+using CollegeProjectMVC.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
 
 namespace CollegeProjectMVC.Tests.Controllers
 {
     [TestClass]
     public class ModuleUnitTest
     {
-        [TestMethod]
+        //[TestMethod]
 
-            //public void ModuleName()
-            //{
-            //    // Arrange
-            //    CourseController controller = new CourseController();
+        //public void ModuleName()
+        //{
+        //    // Arrange
+        //    ComputerScienceController controller = new ComputerScienceController();
 
-            //    //Act
-            //    ViewResult viewResult = controller.Course();
-            //    CourseModel result = viewResult.Model as CourseModel;
+        //    //Act
+        //    ViewResult viewResult = controller.Course();
+        //    CourseModel result = viewResult.Model as CourseModel;
 
-            //    // Assert
-            //    Assert.AreEqual("Computer Science", result.CourseTitle);
-            //}
-        }
+        //    // Assert
+        //    Assert.AreEqual("Computer Science", result.CourseTitle);
+        //}
     }
+    
 }
