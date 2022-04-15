e89b5de32b2a42357e40e58b6756733a3574982b
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-04-15 12:47:33 +0100

    All Testing Done

---
+++ Project/CollegeProjectMVC.Tests/Controllers/RegisterandLoginTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CollegeProjectMVC;
+using CollegeProjectMVC.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace CollegeProjectMVC.Tests.Controllers
+{
+    [TestClass]
+    public class RegisterandLoginTest
+    {
+        [TestMethod]
+        public void Register()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.Register() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void ForgotPassword()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.ForgotPassword() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void FPasswordConfirmation()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.ForgotPasswordConfirmation() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void ExternalLoginFailure()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.ExternalLoginFailure() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
