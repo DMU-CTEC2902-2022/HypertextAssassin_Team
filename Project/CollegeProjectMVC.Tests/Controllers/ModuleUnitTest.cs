deaa586931b4065b52772ef846b91aa7b37ac7aa
No3Mc <41834061+No3Mc@users.noreply.github.com>
2022-04-15 12:03:14 +0100

    Module Testing done

--- Project/CollegeProjectMVC.Tests/Controllers/ModuleUnitTest.cs
+++ Project/CollegeProjectMVC.Tests/Controllers/ModuleUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using CollegeProjectMVC;
 using CollegeProjectMVC.Controllers;
+using CollegeProjectMVC.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -12,20 +13,80 @@ namespace CollegeProjectMVC.Tests.Controllers
     [TestClass]
     public class ModuleUnitTest
     {
-        //[TestMethod]
+        [TestMethod]
 
-        //public void ModuleName()
-        //{
-        //    // Arrange
-        //    ComputerScienceController controller = new ComputerScienceController();
+        public void ModuleCode()
+        {
+            // Arrange
+            ComputerScienceController controller = new ComputerScienceController();
 
-        //    //Act
-        //    ViewResult viewResult = controller.Course();
-        //    CourseModel result = viewResult.Model as CourseModel;
+            //Act
+            ViewResult viewResult = controller.Modules();
+            ComputerScience result = viewResult.Model as ComputerScience;
 
-        //    // Assert
-        //    Assert.AreEqual("Computer Science", result.CourseTitle);
-        //}
+            // Assert
+            Assert.AreEqual("COS1903 ", result.ModuleCode);
+        }
+        [TestMethod]
+        public void ModuleTitle()
+        {
+            // Arrange
+            ComputerScienceController controller = new ComputerScienceController();
+
+            //Act
+            ViewResult viewResult = controller.Modules();
+            ComputerScience result = viewResult.Model as ComputerScience;
+
+            // Assert
+            Assert.AreEqual("Scala Programming.", result.ModuleTitle);
+        }
+        [TestMethod]
+        public void ModuleDescription()
+        {
+            // Arrange
+            ComputerScienceController controller = new ComputerScienceController();
+
+            //Act
+            ViewResult viewResult = controller.Modules();
+            ComputerScience result = viewResult.Model as ComputerScience;
+
+            // Assert
+            Assert.AreEqual("Scala is a strong statically typed general-purpose programming language which supports both object-oriented programming and functional programming. Designed to be concise, many of Scala's design decisions are aimed to address criticisms of Java.", result.ModuleDescription);
+        }
+        [TestMethod]
+        public void ModuleContents()
+        {
+            // Arrange
+            ComputerScienceController controller = new ComputerScienceController();
+
+            //Act
+            ViewResult viewResult = controller.Modules();
+            ComputerScience result = viewResult.Model as ComputerScience;
+
+            // Assert
+            Assert.AreEqual("Scala Coding, Java Virtual Machine, and OOP.", result.ModuleContents);
+        }
+        [TestMethod]
+        public void StaffMembers()
+        {
+            // Arrange
+            ComputerScienceController controller = new ComputerScienceController();
+
+            //Act
+            ViewResult viewResult = controller.Modules();
+            ComputerScience result = viewResult.Model as ComputerScience;
+
+            // Assert
+            Assert.AreEqual("Dr.Alber, Dr.Christopher, Dr.Liam.", result.StaffMembers);
+        }
+
+
+
+
+
+
+
+
     }
     
 }
